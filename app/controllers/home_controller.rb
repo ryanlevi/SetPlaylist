de3eaa1c9a272726a25e23b874bd12722e5c6be2
Ryan <ryan.e.levi@gmail.com>
2014-07-21 21:08:31 -0700

    bootstrap + homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @featured_events = []
+    @featured_events.push HTTParty.get("http://api.songkick.com/api/3.0/events/19369714.json?apikey=UqncZSiBYPTv4CI0").parsed_response["resultsPage"]["results"]["event"]
+    @featured_events.push HTTParty.get("http://api.songkick.com/api/3.0/events/19260149.json?apikey=UqncZSiBYPTv4CI0").parsed_response["resultsPage"]["results"]["event"]
+    @featured_events.push HTTParty.get("http://api.songkick.com/api/3.0/events/16767699.json?apikey=UqncZSiBYPTv4CI0").parsed_response["resultsPage"]["results"]["event"]
   end
   def playlist
     @setlist = GetSetlist.new(params[:artist]).setlist
