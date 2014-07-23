415a7f75e5754b36ebcfb41cacc873c844ac17f9
Ryan <ryan.e.levi@gmail.com>
2014-07-22 22:03:03 -0700

    normalizing text so artists like beyoncE and tiEsto don't cause errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,12 +6,13 @@ class HomeController < ApplicationController
     @featured_events.push HTTParty.get("http://api.songkick.com/api/3.0/events/16767699.json?apikey=UqncZSiBYPTv4CI0").parsed_response["resultsPage"]["results"]["event"]
   end
   def playlist
-    @obj = GetSetlist.new(params[:artist])
+    artist = ActiveSupport::Inflector.transliterate params[:artist]
+    @obj = GetSetlist.new(artist)
     @setlist = @obj.setlist
     @youtube_urls = []
     @youtube_titles = []
     @setlist.each do |track|
-      url = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet%20&q=#{track['name'].gsub(/ /, '+')}+#{params[:artist].gsub(/ /, '+')}&type=video%20&key=AIzaSyCBGQLzcTJ5zjLLG02IcupdqsW93JeKNZA").parsed_response["items"][0]["id"]["videoId"]
+      url = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet%20&q=#{ActiveSupport::Inflector.transliterate(track['name']).gsub(/ /, '+')}+#{artist.gsub(/ /, '+')}&type=video%20&key=AIzaSyCBGQLzcTJ5zjLLG02IcupdqsW93JeKNZA").parsed_response["items"][0]["id"]["videoId"]
       if url
         @youtube_urls.push url
         @youtube_titles.push track['name']
